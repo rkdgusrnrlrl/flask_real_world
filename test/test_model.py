4fe580a47e374a8fa174e04fbc21c9b7a5d3a7f9
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-08-09 15:14:35 +0900

    feat : extract user model

--- test/test_model.py
+++ test/test_model.py
@@ -1,26 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model.user import User
+from model.user import Base
 import pytest
-
-Base = declarative_base()
-
-
-class User(Base):
-    __tablename__ = 'users'
-    uid = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(50))
-    token = Column(String(50))
-    name = Column(String(50))
-    bio = Column(Text)
-    image_url = Column(String(50))
-
-    def __init__(self, email, token, name, bio, image_url):
-        self.email = email
-        self.token = token
-        self.name = name
-        self.bio = bio
-        self.image_url = image_url
 
 
 @pytest.fixture(scope="session")
