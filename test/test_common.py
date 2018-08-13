91af3a764328b025d6008e4038b579932d9b5f8d
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-08-13 22:57:59 +0900

    feat : add regex test to parse Authorization in Headers

--- test/test_common.py
+++ test/test_common.py
@@ -1,6 +1,19 @@
 from common.common import make_token
+import re
+
 
 def test_make_token():
     some = make_token()
     assert type("") == type(some)
     assert "" != some
+
+
+def test_regex():
+    auth = "Token 123"
+    regex = re.compile(r"^Token ([a-z0-9]+)")
+    match = regex.search(auth)
+
+    assert match is not None
+
+    token = match.group(1)
+    assert token == "123"
